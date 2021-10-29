2b48eb47ec232bf592dca0b854b69621e9de5988
r2d2 <aver.serg1870@gmail.com>
2021-10-29 14:17:00 +0300

    added class Footer

--- home/models.py
+++ home/models.py
@@ -1,37 +1,71 @@
 from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
 
 from wagtail.core.models import Page
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class Footer(models.Model):
+
+    address = RichTextField(
+        features=['enter'],
+        max_length=100,
+        verbose_name="Адрес предприятия",
+    )
+
+    telephones = models.CharField(
+        max_length=100,
+        verbose_name="Телефоны предприятия",
+    )
+
+    email = models.EmailField(
+        verbose_name="Email предприятия",
+    )
+
+    panels = [
+        FieldPanel('address'),
+        FieldPanel('telephones'),
+        FieldPanel('email'),
+    ]
+
+    class Meta:
+        verbose_name = 'Футер'
+        verbose_name_plural = 'Футеры'
+
+    def __str__(self):
+        return 'Футер'
 
 
 class PhotoGalleryPage(Page):
     """Фотогалерея"""
-    pass
+    max_count = 1
 
 
 class ContactsPage(Page):
     """Контакты"""
-    pass
+    max_count = 1
 
 
 class FormPage(Page):
     """Сделать заказ"""
-    pass
+    max_count = 1
 
 
 class FagPage(Page):
     """Вопросы и ответы"""
-    pass
+    max_count = 1
 
 
 class LibraryPage(Page):
     """Библиотека"""
-    pass
+    max_count = 1
 
 
 class AboutPage(Page):
     """О компании"""
-    pass
+    max_count = 1
 
 
 class HomePage(Page):
